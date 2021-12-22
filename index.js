305bcdebe54fcae8db064ab2995c6ce168c15351
superdev0505 <grantlatest@gmail.com>
2021-12-22 22:23:31 +0000

    change alert dialog to alert content

--- index.js
+++ index.js
@@ -5,9 +5,25 @@ $(document).ready(function() {
         $.post('/', data, function (res) {
             console.log(res)
         }).done(function(res) {
-            alert( res );
+            const alert_content = `
+            <div class="alert alert-primary" role="alert">
+                ${res}
+            </div>
+            `
+            $('#alert_container').html(alert_content);
+            setTimeout(function(){
+                $('#alert_container').html('');
+            }, 3000);
         }).fail(function(e) {
-            alert( e.responseText );
+            const alert_content = `
+            <div class="alert alert-danger" role="alert">
+                ${e.responseText}
+            </div>
+            `
+            $('#alert_container').html(alert_content);
+            setTimeout(function(){
+                $('#alert_container').html('');
+            }, 3000);
         })
     })
 })
